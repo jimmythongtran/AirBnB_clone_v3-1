e3070ca228b99c479a516158ad708deb027c9750
Incitatous <dcianberlin@gmail.com>
2017-05-04 17:19:55 -0700

    proper dir

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,14 @@
+$( document ).ready(function( {
+  let storeDict = {};
+  $('.small-list li input').change(function() {
+    if $('.small-list li input').checked {
+      storeList[($(this).attr('data-id'))] = $(this).attr('data-name');
+    }
+    else {
+      delete storeDict[($(this).attr('data-id'))]
+    };
+  let totalString = Object.values(storeDict).join(', ');
+  $('.amenities H4').text(totalString);
+  })
+);
+
