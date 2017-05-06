ca131eeee11608a2a2344f238f14ac22cb32dfea
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-05-06 02:23:12 +0000

    it's a dot

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,9 +1,10 @@
 $(document).ready(function () {
   const url = 'http://0.0.0.0:5001/api/v1/status/';
 
-  $.get(url, function (response) {
+  $.get(url, function (response, textStatus) {
     if (response.status === 'OK') {
-      $('DIV#api_status').addClass('.available');
+		alert(response.status);
+      $('DIV#api_status').addClass('available');
     } else {
       $('DIV#api_status').removeClass('available');
     }
